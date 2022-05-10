80a7bea6d8a588e5edcb4eef06005965c9392ecf
Steven Rivera <ghostszmusic@gmail.com>
2022-05-10 16:27:56 -0500

    second

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const app = express();
-require("dotenv").config();
+const path = require('path');
+require('dotenv-flow').config({path: path.resolve(__dirname, `.env.${process.env.NODE_ENV}`)} );
 const host = process.env.HOST;
 const port = process.env.PORT;
 const pool = require("./db")
 
 app.use(express.json());
 
+app.post('/ample-api/login', (req, res) => {
+    let data = [];
+    let queryData = [];
 
+    pool
+        .query('SELECT $1::text as name FROM login', queryData)
+        .then(res => console.log(res))
+        .catch(err => console.error('Error executing query', err.stack))
+
+    res.send(data);
+})
+
+app.post('/ample-api/stock-list', (req, res) => {
+    let data = [];
+
+    res.send(data);
+})
 
 app.listen(port, () =>
     console.log(`Server is listening on port ${host}:${port}!`)
--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const Pool = require('pg').Pool;
-require("dotenv").config();
+require("dotenv-flow").config();
 
 const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: true
 })
 
 module.exports = pool;
